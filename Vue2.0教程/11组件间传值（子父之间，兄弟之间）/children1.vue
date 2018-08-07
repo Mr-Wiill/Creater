21c4b7000a184baa804c1a86183eeed575baa6c4
KBeginner <wyyking@126.com>
2018-08-07 14:33:53 +0800

    组件间传值

---
+++ Vue2.0教程/11组件间传值（子父之间，兄弟之间）/children1.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h3>children1</h3>
+    <h1>{{getData}}</h1>      <!--父组件传过来的值，在这展示-->
+    <input type="text" v-model="toBrother" >
+    <button @click="dataToBrother">toBrother</button>
+  </div>
+</template>
+
+<script>
+  import bus from '../eventBus/bus'
+    export default {
+        name: "children",
+      props:['getData'],    //子组件自定义一个属性
+      data(){
+          return {
+            toBrother:''
+          }
+      },
+      methods:{
+        dataToBrother(){
+          bus.$emit('toBrother',this.toBrother)
+        }
+      }
+    }
+</script>
+
+<style scoped>
+  h1{
+     height: 50px;
+    color: darkred;
+   }
+</style>
---
+++ Vue2.0教程/11组件间传值（子父之间，兄弟之间）/children2.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h3>children1</h3>
+    <h1>{{dataFromBrother}}</h1>
+    <input type="text" v-model="toParentData">
+    <button @click="dataToParent">toParent</button>
+  </div>
+</template>
+
+<script>
+  import bus from '../eventBus/bus'
+    export default {
+        name: "children2",
+      data(){
+          return {
+            toParentData:'',
+            dataFromBrother:''
+          }
+      },
+      created(){
+          bus.$on('toBrother',(data)=>{
+            this.dataFromBrother = data;
+          })
+      },
+      methods:{
+        dataToParent(){
+          this.$emit('toParent',this.toParentData)
+        }
+      }
+    }
+</script>
+
+<style scoped>
+  h1{
+    height: 50px;
+    color: darkred;
+  }
+</style>
