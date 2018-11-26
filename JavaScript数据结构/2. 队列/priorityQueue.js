a0453a3bf1e37008bb5bffbb72f090876c46dd2e
KBeginner <wyyking@126.com>
2018-11-26 22:40:16 +0800

    queue and priority

--- JavaScript数据结构/2. 队列/priorityQueue.js
+++ JavaScript数据结构/2. 队列/priorityQueue.js
@@ -1,6 +1,30 @@
+/* jshint esversion:6 */
 
+/* 定义一个优先队列类 */
 function PriorityQueue(){
-    let q = new Queue();
-    q.enqueue(1);
-    return eq.getQueue();
+    // 优先队列数组
+    let items = [];
+    // 定义一个辅助类（用于创建一项具有优先级的元素）
+    function AuxiliaryClass(element,priority){
+        this.element = element;         // 元素名称
+        this.priority = priority;       // 优先级标识
+    }
+    // 优先队列元素入列的方法
+    this.enqueue = (element,priority)=>{
+        let queueItem = new AuxiliaryClass(element,priority);       // 调用辅助类创建一个元素
+        let added = false;
+        for(let i=0,len=items.length; i<len; i++){      
+            if(queueItem.priority > items[i].priority){     // 与每一项做比较，若优先级比较高，则插入其前面，结束比较
+                items.splice(i,0,queueItem);
+                added = true;
+                break;
+            }
+        }
+        if(!added){             // 比队列的所有项都小，则插入尾部
+            items.push(queueItem);
+        }
+    };
+    this.getPriorityQueue = ()=>{
+        return items;
+    };
 }
