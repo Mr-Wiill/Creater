8961f80a12c5038e69ce91cdc9f7c4fd11725129
KBeginner <wyyking@126.com>
2018-12-02 14:31:42 +0800

    skipper

--- JavaScript数据结构/4. 集合（Skipper）/skipper.js
+++ JavaScript数据结构/4. 集合（Skipper）/skipper.js
@@ -2,8 +2,25 @@
 /* 定义一个集合类 */
 let Skipper = ()=>{
     let item = {};
-    // has(element) 查询集合是否存在某项
+    // has(element) 查询集合是否存在某元素
     this.has = (element)=>{
+        return item.hasOwnProperty(element);
+    };
+    // add(element) 向集合里添加新元素
+    this.add = (element)=>{
+        if(!this.has(element)){
+            item[element] = element;
+        }
+        return '该元素已存在';
+    };
 
+    // delete(element) 删除某个元素
+    this.delete = (element)=>{
+        if(this.has(element)){
+            delete item.delete;
+        }else{
+            return '该元素不存在';
+        }
     }
+
 }
