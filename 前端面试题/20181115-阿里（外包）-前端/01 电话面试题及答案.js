f378f0117fd6f5d24137d93ca9f6a8b7989bf0af
KBeginner <wyyking@126.com>
2018-12-17 23:05:28 +0800

    并集，交集和差集

--- 前端面试题/20181115-阿里（外包）-前端/01 电话面试题及答案.js
+++ 前端面试题/20181115-阿里（外包）-前端/01 电话面试题及答案.js
@@ -31,6 +31,7 @@
 
 5、MVVM模式介绍
    MVVM 是Model-View-ViewModel 的缩写，它是一种基于前端开发的架构模式，其核心是提供对View 和 ViewModel 的双向数据绑定，这使得ViewModel 的状态改变可以自动传递给 View，即所谓的数据双向绑定
+   详细介绍：https://segmentfault.com/a/1190000006599500?utm_source=tag-newest
 
 6、Vue生命周期
    beforeCreate()   创建组件
