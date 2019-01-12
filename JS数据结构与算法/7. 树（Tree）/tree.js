f43692822ee72b53b5ffab0b35ba5a11dbf54e77
KBeginner <wyyking@126.com>
2019-01-12 16:51:01 +0800

    tree数据结构

--- JS数据结构与算法/7. 树（Tree）/tree.js
+++ JS数据结构与算法/7. 树（Tree）/tree.js
@@ -38,4 +38,89 @@ class Tree{
             insertNode(this.root,newNode)
         }
     }
+    /* 遍历节点 */
+    traverse(){
+        const print = (node)=>{
+            console.log(node.value)      // 打印节点的值       
+            // return node.value                   
+        }
+        /* 遍历节点（递归） */
+        const traverseNode = (node,callback)=>{
+            if(node == null) return    // 如果节点为null，退出递归
+            // callback(node)                    // 前遍历（从上到下打印）
+            traverseNode(node.left,callback)     // 遍历左节点
+            // callback(node)                    // 中遍历（从中间开始印打印）
+            traverseNode(node.right,callback)    // 遍历右节点
+            callback(node)             // 后遍历（从下到上打印）
+        }
+        traverseNode(this.root,print)
+    }
+    /* 获取最小值 */
+    min(){
+        // 递归方法
+        /* const getMin = (node)=>{
+            if(node == null){
+                return null
+            } else{
+                if(node.left == null){
+                    console.log(node.value)
+                    // return node.value
+                }else{
+                    getMin(node.left)
+                }
+            }
+        }
+        getMin(this.root) */
+
+        // 循环方法
+        const getTreeMin = (node)=>{
+            if(!node) return null
+            while(node && node.left){      // 一直往左查找，直到找到最左节点
+                node = node.left
+            }
+            console.log(node.value)
+            return node.value
+        }
+        getTreeMin(this.root)
+    }
+    /* 获取最大值 */
+    max(){
+        const getMax = (node)=>{
+            if(!node) return null
+            while(node && node.right){
+                node = node.right
+            }
+            console.log(node.value)
+            return node.value
+        }
+        getMax(this.root)
+    }
+    /* 移除节点（重构树） */
+    remove(){
+        const removeNode = (node,value)=>{
+            if(value < node.value){             /* 继续向左查找 */
+                node.left = removeNode(node.left,value)         
+                return node         // 返回重构后的树
+            } 
+            else if(value > node.value){        /* 继续向右查找 */
+                node.right = removeNode(node.right,value)
+                return node
+            } 
+            else{      /* node.value == value （直接删除node节点）*/
+                if(node.left == null && node.right == null){      // 叶子节点（没有子级）
+                    node = null
+                    return node
+                }
+                else if(node.left == null && node.right){         // 只有一个右节点
+                    return node.right
+                }
+                else if(node.left && node.right == null){         // 只有一个左节点
+                    return node.left
+                }
+                else{                                             // 有两个子节点
+                    
+                }
+            }
+        }
+    }
 }
