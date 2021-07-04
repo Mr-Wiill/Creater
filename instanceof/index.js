7c591d3975c32f4bbda9c53baca19877e7e379a7
KBeginner <wyyking@126.com>
2021-07-04 19:56:15 +0800

    instanceof原理

---
+++ instanceof/index.js
@@ -0,0 +1,11 @@
+// A instanceof B判断数据类型的原理
+/* 判断A的原型是否在B的原型链上 */
+function instanceof(A, B) {
+    let left = A.__proto__
+    let right = B.prototype
+    while (true) {
+        if (left == null) return false
+        if (left == right) return true
+        left = left.__proto__
+    }
+}
