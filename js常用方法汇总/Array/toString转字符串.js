549726c667b3abb7925e3138825d718c8069cae0
KBeginner <wyyking@126.com>
2022-02-28 13:34:10 +0800

    js常用方法

---
+++ js常用方法汇总/Array/toString转字符串.js
@@ -0,0 +1,9 @@
+/* 
+    toString（）
+    可把值转换成字符串。
+*/
+
+const arr = [3,4,4,5,4,6,5,7];
+console.log(arr.toString()); // 3,4,4,5,4,6,5,7
+
+
---
+++ js常用方法汇总/Array/valueOf返回本身.js
@@ -0,0 +1,8 @@
+/* 
+    valueOf（）
+    返回数组本身。
+*/
+
+const arr = [3,4,4,5,4,6,5,7];
+console.log(arr.valueOf()); // [3,4,4,5,4,6,5,7]
+
