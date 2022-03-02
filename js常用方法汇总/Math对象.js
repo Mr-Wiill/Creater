25b3567ac972a23bafff5db4599ac7357649c17f
KBeginner <wyyking@126.com>
2022-03-02 18:08:43 +0800

    Math数学对象

---
+++ js常用方法汇总/Math对象.js
@@ -0,0 +1,43 @@
+/* 
+
+Math.ceil():  对数进行上舍入（天花板函数）
+
+Math.floor(): 对数进行下舍入（地板函数）。
+
+Math.max(x,y): 返回x,y中的最大值。
+
+Math.min(x,y): 返回x,y中的最小值。
+
+Math.pow(x,y): 返回x的y次方。
+
+Math.random(): 返回0-1之间的随机数。
+
+Math.round(x): 四舍五入。
+
+Math.abs(x): 返回数的绝对值。
+
+Math.acos(x): 返回数的反余弦值。
+
+Math.asin(x): 返回数的反正弦值。
+
+Math.atan(x): 返回数的反正切值。
+
+Math.atan2(y,x): 返回由x轴到点（x，y）的角度（以弧度为单位）。
+
+Math.cos(x): 返回数的余弦值。
+
+Math.exp(e): 返回e的指数。
+
+Math.log(x): 返回数的自然对数（以e为底数）。
+
+Math.sin(x): 返回数的正弦值。
+
+Math.sqrt(x): 返回数的平方根。
+
+Math.tan(x): 返回角的正切值。
+
+Math.toSource(): 返回该对象的源代码。
+
+Math.valueOf(): 返回Math对象的原始值。
+
+*/
