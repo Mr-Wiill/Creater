404424137e7977b9ef49de343b18fda5579cc705
KBeginner <wyyking@126.com>
2022-03-06 22:54:29 +0800

    柯里化

---
+++ interview/柯里化/what is Currying.js
@@ -0,0 +1,33 @@
+/* 
+维基百科上说道：柯里化，英语：Currying(果然是满满的英译中的既视感)，是把接受多个参数的函数变换成接受一个单一参数（最初函数的第一个参数）的函数，并且返回接受余下的参数而且返回结果的新函数的技术。
+
+Currying有哪些好处呢?
+1、参数复用；
+2、提前确认；
+3、延迟运行。
+*/
+
+
+// 正常正则验证字符串 reg.test(txt)
+
+// 函数封装后
+function check(reg, txt) {
+    return reg.test(txt)
+}
+
+check(/\d+/g, 'test')       //false
+check(/[a-z]+/g, 'test')    //true
+
+// Currying后
+function curryingCheck(reg) {
+    return function (txt) {
+        return reg.test(txt)
+    }
+}
+
+var hasNumber = curryingCheck(/\d+/g)
+var hasLetter = curryingCheck(/[a-z]+/g)
+
+hasNumber('test1')      // true
+hasNumber('testtest')   // false
+hasLetter('21212')      // false
